41479bf3bfe365d31dcab18d77899eb154e02867
shruti629 <shrutikumaridss@gmail.com>
2025-11-08 18:26:46 +0530

    update

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def get_requirements(file_path:str) -> List[str]:
     return requirements
 
 setup(
-    name="ml_project",
+    name="student performance Analysis",
     version="0.1",
     author="shruti",
     author_email="shruti_2312res629@iitp.ac.in",
